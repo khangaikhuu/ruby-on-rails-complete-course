b2791cda995c576fbaaa4a3be75a541360a5e5b8
khangaikhuu <khangaikhuu@googlemail.com>
2021-05-13 22:05:07 +0800

    [ADD] updating student.rb

--- Section-2/student.rb
+++ Section-2/student.rb
@@ -1,22 +1,27 @@
 class Student
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  attr_accessor :first_name, :last_name, :email, :username, :password
 
-  def to_s
-    "First name: #{@first_name}"
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @email = email
+    @username = username
+    @password = password
   end
 
-  def first_name(name)
-    @first_name = name
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name},Username: #{@username}, Email: #{@email} "
   end
 
 
 end
 
-khangai = Student.new
-puts khangai
-khangai.first_name("Khangai")
-puts khangai
+khangai = Student.new("Khangaikhuu", "Uvgunkhuu","khangai", "khangaikhuu@protonmail.com", "password1")
+
+john = Student.new("John", "Doe","john1", "john@protonmail.com", "password2")
+
+puts khangai 
+puts john
+
+
+
