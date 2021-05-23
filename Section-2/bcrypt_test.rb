f5822a3cac4986a28cd4dacc820f734a74be0d61
khangaikhuu <khangaikhuu@googlemail.com>
2021-05-23 17:33:06 +0800

    [ADD] crud.rb for creating hash from password using bcrypt algorithm

---
+++ Section-2/bcrypt_test.rb
@@ -0,0 +1,9 @@
+require 'bcrypt'
+
+my_password = BCrypt::Password.create("my password")
+my_password_1 = BCrypt::Password.create("my password")
+my_password_2 = BCrypt::Password.create("my password")
+
+puts my_password == "my password"     #=> true
+puts my_password == "not my password" #=> false
+
